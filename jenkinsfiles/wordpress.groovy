84a2d161ef61faa8ee5b3e9481d7532d3cc346ca
sahilmahale11 <139956087+sahilmahale11@users.noreply.github.com>
2023-08-02 19:15:31 +0530

    Update wordpress.groovy

--- jenkinsfiles/wordpress.groovy
+++ jenkinsfiles/wordpress.groovy
@@ -18,7 +18,7 @@ pipeline {
 	    					          
             stage('mysql deployment') {
 		sshagent(credentials: ['local1']) {
-			  steps {
+			  
 			    sh '''kubectl apply -f wordpre-phpmysql-mysql-deployments/mysql-user-pass.yaml
 	              kubectl apply -f wordpre-phpmysql-mysql-deployments/mysql-db-url.yaml
                 kubectl apply -f wordpre-phpmysql-mysql-deployments/mysql-root-pass.yaml
@@ -26,19 +26,19 @@ pipeline {
                 kubectl apply -f wordpre-phpmysql-mysql-deployments/mysql-pvc.yaml
                 kubectl apply -f wordpre-phpmysql-mysql-deployments/mysql-deployment.yaml
                 kubectl apply -f wordpre-phpmysql-mysql-deployments/mysql-service.yaml'''		         
-	                 }
+	                 
 			    }
           stage('php-myadmin-deployment') {
-			     steps {
+			     
 			          sh '''kubectl apply -f wordpre-phpmysql-mysql-deployments/phpmyadmin-deploy.yaml
 	              kubectl apply -f wordpre-phpmysql-mysql-deployments/phpmyadmin-service.yaml'''         
-	                 }
+	                
 			    }
           stage('wordpress-deployment') {
-			     steps {
+			  
 			          sh '''kubectl apply -f wordpre-phpmysql-mysql-deployments/wordpress-deploy.yaml
 	              kubectl apply -f wordpre-phpmysql-mysql-deployments/wordpress-service.yaml'''         
-	                 }
+	                
 	                   }
 			    }
           }
